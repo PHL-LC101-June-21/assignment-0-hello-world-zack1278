8e667761410cacd9e10af92ca1a58b0d01719f48
zach127 <>
2021-06-08 23:49:32 +0000

    i updated hello.js to say hello world

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
 function hello() {
-	// TODO: change this string so that your program prints "Hello world!" 
-	// when you hit the replit run button
-	return "Hello!";
+
+	return "Hello world!";
 }
  
 module.exports = hello;
